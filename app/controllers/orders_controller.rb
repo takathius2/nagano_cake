f737434d661c750e1cecdd733de1d0ae9fa4d275
EC2 Default User <ec2-user@ip-172-31-8-216.ap-northeast-1.compute.internal>
2022-10-01 11:41:22 +0000

    [Add]顧客情報編集機能実装済み

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,21 @@
+class OrdersController < ApplicationController
+  def new
+    @order = Order.new
+    @
+  end
+  
+  def comfirm
+  end
+
+  def complete
+  end
+  
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,45 @@
 Rails.application.routes.draw do
  
+  get '/' => 'homes#top'
+  get '/about' => 'homes#about'
+  
+ #管理者情報関連
   devise_for :admin, skip:[:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
   
-  get '/' => 'homes#top'
-  get '/about' => 'homes#about'
-  #customer用itemのパス
-  resources :items, only: [:index, :show]
-  namespace :admin do
-    get '/' => 'homes#top'
     
-    resources :items, only: [:new, :create, :index, :show, :edit, :update]
-    resources :genres, only: [:index, :edit, :create, :update]
-  end
-    
-  #顧客用
+  #顧客情報関連
   devise_for :customers,skip: [:passwords], controllers: {
       registrations: "public/registrations",
       sessions: 'public/sessions'
   }
-
-  #顧客情報関連
+  #顧客のページ関連
   get '/customers/my_page' => 'customers#show'
   get '/customers/infomation/edit' => 'customers#edit'
   patch '/customers/infomation/up_date' => 'customers#update'
   get '/customers/unsubscribe' => 'customers#unsubscribe'
   patch '/customers/withdraw' => 'customers#withdraw'
+  
+  #商品関連
+  resources :items, only: [:index, :show]
+  namespace :admin do
+    get '/' => 'homes#top'
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+    resources :genres, only: [:index, :edit, :create, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    patch '/customers' => 'customer#update'
+    parch '/order_details/id' => 'order_detail#update'
+  end
+  #注文関連
+  resources :orders, only: [:new, :index, :show]
+  post '/orders/comfirm' => 'orders#comfirm'
+  get '/orders/complete' => 'orders#complete'
+  post '/orders' => 'orders#create'
+  
   #カート関連
-
   delete '/cart_items/destroy_all' => 'cart_items#destroy_all', as: 'destroy_all_cart_items'
   resources :cart_items, only: [:index, :update, :destroy, :create]
-   # 管理者用
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
